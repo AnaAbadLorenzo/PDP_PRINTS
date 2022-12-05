cf052053a7a6c554fa376e091cf3b8d97904d3aa
martin21d <mpgonzalez18@esei.uvigo.es>
2022-12-05 12:22:07 +0100

    add persona

--- PDP/REST/Back/Servicios/GestionPersonas/GestionPersonasService.php
+++ PDP/REST/Back/Servicios/GestionPersonas/GestionPersonasService.php
@@ -4,10 +4,12 @@ interface GestionPersonasService {
 
 	public function inicializarParametros($accion);
 
+    function add($mensaje);
 	function edit($mensaje);
     function delete($mensaje);
     function search($mensaje);
     function searchByParameters($mensaje);
+    
     //function searchByDNI($mensaje);
 	
 }
